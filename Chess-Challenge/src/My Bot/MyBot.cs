b5d4020e15b0bebc15181da6386783a35a6a6c46
Mark <markrobertvandam@gmail.com>
2023-07-31 14:30:52 +0200

    first working version

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,96 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
+
+    public int eval(IEnumerable<PieceList> pieces)
+    {
+        var t = pieces.Select(c => c.Count);
+        return pieces.Select(c => c.Count * pieceValues[(int)c.TypeOfPieceInList]).Sum();   
+    }
+
+    public int eval_after_move(Board board, Move move)
+    {
+        board.MakeMove(move);
+        int evaluation = eval(board.GetAllPieceLists().Where(c => c.IsWhitePieceList == board.IsWhiteToMove));
+
+        board.UndoMove(move);
+
+        return evaluation;
+    }
+
+    public Move ThinkGen(Board board, Timer timer, bool isBot)
+    {
+        Move[] allMoves = board.GetLegalMoves();
+        List<Tuple<Move, int>> candidateMoves = new List<Tuple<Move, int>>();
+
+        // Pick a random move to play if nothing better is found
+        Random rng = new();
+        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
+        int highestValueCapture = 0;
+
+        foreach (Move move in allMoves)
+        {
+            // Always play checkmate in one
+            if (MoveIsCheckmate(board, move))
+            {
+                return move;
+            }
+
+            if (isBot)
+            {
+                var initial_eval = eval(board.GetAllPieceLists().Where(c => c.IsWhitePieceList == board.IsWhiteToMove));    
+                // try move 
+                board.MakeMove(move);
+
+                // See what move enemy wants to make
+                Move enemy_move = ThinkGen(board, timer, false);
+
+                int updated_eval = eval_after_move(board, enemy_move);
+                board.UndoMove(move);
+
+                candidateMoves.Add(new(move, updated_eval));
+            } 
+            else
+            {
+                // Find highest value capture
+                Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+                if (capturedPieceValue > highestValueCapture)
+                {
+                    moveToPlay = move;
+                    highestValueCapture = capturedPieceValue;
+                }
+            }
+        }
+
+        if (isBot)
+        {
+            var sorted_CandidateMoves = candidateMoves.OrderByDescending(t => t.Item2).ToList();
+            moveToPlay = sorted_CandidateMoves[0].Item1;
+        }
+
+        return moveToPlay;
+    }
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        return ThinkGen(board, timer, true);
+    }
+
+    // Test if this move gives checkmate
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
